111991537a95f33f0d169409dcaab02129cfde7f
Robinson Besson <robinson.besson@gmx.com>
2018-11-14 08:29:35 +0100

    merge Loan
    Rajouté code Loan.
    Edit commentaires.
    Rajouté chargement fichier (en cours)

--- source/main.c
+++ source/main.c
@@ -10,16 +10,17 @@
 
 /**
  *	Liste programmes:
- *		1- Ajouter Ville
- *		2- Modifier Ville
- *		3- Supprimer Ville
- *		4- Ajouter une nouvelle livraison dans une Ville
- *		5- Supprimer une livraison dans une Ville
- *		6- Modifier livraison dans une Ville
- *		7- Recherche dans une Ville une certaine capacité
- *		8- Recherche n'importe quelle Ville une certaine capacité
- *		9- Transfert d'un chauffeur d'une Ville 1 à une Ville 2
-**/
+ *		1	Ajouter Ville
+ *		2	Modifier Ville
+ *		3	Supprimer Ville
+ *		4	Ajouter une nouvelle livraison dans une Ville
+ *		5	Supprimer une livraison dans une Ville
+ *		6	Modifier livraison dans une Ville
+ *		7	Recherche dans une Ville une certaine capacité
+ *		8	Recherche n'importe quelle Ville une certaine capacité
+ *		9	Transfert d'un chauffeur d'une Ville 1 à une Ville 2
+ *		10	Enregistrer dans un fichier
+ **/
 
 
 
@@ -65,6 +66,61 @@ typedef tLivraison * ptLivraison;	// Définition type pointeur vers struct tLivra
 
 
 
+// Afficher Liste Ville
+void afficherListeVille(ptVille px) {
+	printf("Liste de villes :\n");
+	while (px->villeSuivante != NULL) {
+		printf("%d\n", px->numVille);
+		px = px->villeSuivante;
+	}
+	printf("Fin de la liste des villes");
+}
+
+
+// 1- Ajouter Ville
+void ajouterVille(ptVille px, int valVille) {
+	px->villeSuivante->villePrecedente = (ptVille)malloc(sizeof(tVille));
+	px->villeSuivante->villePrecedente->villeSuivante = px->villeSuivante;
+	px->villeSuivante->villePrecedente->villePrecedente = px;
+	px->villeSuivante = px->villeSuivante->villePrecedente;
+	px->villeSuivante->numVille = valVille;
+
+	px->villeSuivante->listeLivraison = NULL;
+}
+
+
+// 2- Modifier numéro Ville
+void modifierVille(ptVille px, int num, int nouvVal) {
+	while (px->villeSuivante->villeSuivante != NULL) {
+		if (px->numVille == num) {
+			px->numVille = nouvVal;
+			break;
+		}
+	}
+}
+
+
+// 3- Supprimer Ville
+void supprimerVille(ptVille px, int num) {
+	while (px->villeSuivante->villeSuivante != NULL) {
+		printf("coucou");
+		if (px->villeSuivante->numVille == num) {
+			px->villeSuivante->villeSuivante->villePrecedente = px;
+			px->villeSuivante = px->villeSuivante->villeSuivante;
+			break;
+		}
+		px = px->villeSuivante;
+	}
+}
+
+
+// 11- Charger structure depuis un fichier
+//void chargerStructure()
+
+
+
+
+
 int main() {
 	
 	// Création des bidons
@@ -93,7 +149,7 @@ int main() {
 	int c = -1;
 	
 	// Boucle menu
-	while (c != 10) {
+	while (c != 20) {
 		printf("1- Ajouter Ville\n");
 		printf("2- Modifier Ville\n");
 		printf("3- Supprimer Ville");
@@ -103,55 +159,23 @@ int main() {
 		printf("7- Recherche dans une Ville une certaine capacite\n");
 		printf("8- Recherche n'importe quelle Ville une certaine capacite\n");
 		printf("9- Transfert d'un chauffeur d'une Ville 1 a une Ville 2\n");
-		printf("10- Quitter\n");
+		printf("10- Enregistrer fichier\n");
+		printf("11- Charger fichier\n");
+		printf("20- Quitter\n");
 		
 		printf("Entrez le numero de la fonction a appeler: ");
 		scanf("%d", &c);
 
 		// Boucle entrée non valide
-		while (c < 0 || c > 10) {
+		while (c < 0 || c > 12) {
 			printf("\n");
 			printf("Entrez le numero de la fonction a appeler: ");
 			scanf("%d", &c);
 		}
 		
 
-		switch (c) {
-			case 1:
-				printf("%d", c);
-				break;
-			
-			case 2:
-				printf("%d", c);
-				break;
-			
-			case 3:
-				printf("%d", c);
-				break;
-			
-			case 4:
-				printf("%d", c);
-				break;
-			
-			case 5:
-				printf("%d", c);
-				break;
-			
-			case 6:
-				printf("%d", c);
-				break;
-			
-			case 7:
-				printf("%d", c);
-				break;
-			
-			case 8:
-				printf("%d", c);
-				break;
-			
-			case 9:
-				printf("%d", c);
-				break;
+		if (c == 11) {
+
 		}
 	}
 	
