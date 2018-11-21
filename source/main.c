eb6443005115ca679ed161e6f3c18b3715709ac6
Robinson Besson <robinson.besson@gmx.com>
2018-11-21 09:27:23 +0100

    Ajouté fonction chargement fichier

--- source/main.c
+++ source/main.c
@@ -4,10 +4,6 @@
  **/
 
 
-
-
-
-
 /**
  *	Liste programmes:
  *		1	Ajouter Ville
@@ -20,15 +16,12 @@
  *		8	Recherche n'importe quelle Ville une certaine capacité
  *		9	Transfert d'un chauffeur d'une Ville 1 à une Ville 2
  *		10	Enregistrer dans un fichier
+ *		11	Charger un fichier
  **/
 
 
 
-
-
 #pragma warning(disable : 4996)	// Désactives les allertes causés par scanf
-
-
 
 
 
@@ -38,11 +31,11 @@
 
 
 
-
 struct Livraison;	// Redéfinit par la suite, nécessaire pour déclarer struct Ville
 
 
-typedef struct Ville {
+typedef struct Ville
+{
 	int numVille;	// Numéro de la ville
 	struct Ville * villeSuivante;
 	struct Ville * villePrecedente;
@@ -50,7 +43,8 @@ typedef struct Ville {
 } tVille;
 
 
-typedef struct Livraison {
+typedef struct Livraison
+{
 	int chauffeur;
 	int capacite;
 	int disponible;
@@ -67,9 +61,11 @@ typedef tLivraison * ptLivraison;	// Définition type pointeur vers struct tLivra
 
 
 // Afficher Liste Ville
-void afficherListeVille(ptVille px) {
+void afficherListeVille(ptVille px)
+{
 	printf("Liste de villes :\n");
-	while (px->villeSuivante != NULL) {
+	while (px->villeSuivante != NULL)
+	{
 		printf("%d\n", px->numVille);
 		px = px->villeSuivante;
 	}
@@ -78,12 +74,16 @@ void afficherListeVille(ptVille px) {
 
 
 // Afficher Liste Livraison
-void afficherListeLivraison(ptVille px, int valVille) {
+void afficherListeLivraison(ptVille px, int valVille)
+{
 	printf("Liste de Livraison :\n");
-	while (px->villeSuivante != NULL) {
-		if (px->numVille == valVille) {
+	while (px->villeSuivante != NULL)
+	{
+		if (px->numVille == valVille)
+		{
 			ptLivraison py = px->listeLivraison;
-			while (py->livraisonSuivante != NULL) {
+			while (py->livraisonSuivante != NULL)
+			{
 				printf("%d\n", py->chauffeur);
 				py = py->livraisonSuivante;
 			}
@@ -94,7 +94,8 @@ void afficherListeLivraison(ptVille px, int valVille) {
 
 
 // 1- Ajouter Ville
-void ajouterVille(ptVille px, int valVille) {
+void ajouterVille(ptVille px, int valVille)
+{
 	px->villeSuivante->villePrecedente = (ptVille)malloc(sizeof(tVille));
 	px->villeSuivante->villePrecedente->villeSuivante = px->villeSuivante;
 	px->villeSuivante->villePrecedente->villePrecedente = px;
@@ -106,9 +107,12 @@ void ajouterVille(ptVille px, int valVille) {
 
 
 // 2- Modifier numéro Ville
-void modifierVille(ptVille px, int num, int nouvVal) {
-	while (px->villeSuivante != NULL) {
-		if (px->numVille == num) {
+void modifierVille(ptVille px, int num, int nouvVal)
+{
+	while (px->villeSuivante != NULL)
+	{
+		if (px->numVille == num)
+		{
 			px->numVille = nouvVal;
 			break;
 		}
@@ -118,9 +122,12 @@ void modifierVille(ptVille px, int num, int nouvVal) {
 
 
 // 3- Supprimer Ville
-void supprimerVille(ptVille px, int num) {
-	while (px->villeSuivante != NULL) {
-		if (px->villeSuivante->numVille == num) {
+void supprimerVille(ptVille px, int num)
+{
+	while (px->villeSuivante != NULL)
+	{
+		if (px->villeSuivante->numVille == num)
+		{
 			px->villeSuivante->villeSuivante->villePrecedente = px;
 			px->villeSuivante = px->villeSuivante->villeSuivante;
 			break;
@@ -131,22 +138,186 @@ void supprimerVille(ptVille px, int num) {
 
 
 // 4- Ajouter une livraison dans une ville
-void ajouteLivraison(int chauffeur, int capacite, int disponible, int valVille) {
+void ajouteLivraison(int chauffeur, int capacite, int disponible, int valVille)
+{
 
 }
 
 
 // 11- Charger structure depuis un fichier
-void chargerStructure() {
-	FILE *f;
-	f = fopen("Toto.dat");
+void chargerStructure(ptVille pdebut, ptVille px)
+{
+	FILE *fich;	// poiteur vers fichier
+	fich = fopen("Toto.dat", "rt");	//ouverture fichier en lecture
+	if (fich == NULL) printf("Probleme d'ouverture du fichier");	// vérifie la bonne ouverture du fichier
+
+	ptVille debut = px;	// pointeur vers début d'insertion
+
+	int hash = -1;		// compte nombre #
+	int doll = 0;		// compte nombre $
+
+	int sec = 0;		// indicateur section
+	int sub = 0;		// indicateur sous-section
+	int champ = 0;		// indicateur champ de données
+
+	char c;
+	int value;
+
+	while (feof(fich) != 0) 
+	{
+		c = fgetc(fich);	// Lecture caractère actif
+
+		if (hash != -1)		// Si on n'est pas au début du fichier,
+		{
+			if (c == '#')
+			{
+				hash++;		// on contabilise les '#'
+			}
+			else if (c == '$')
+			{
+				doll++;		// et les '$'.
+			}
+		}
+
+		if ( ( c >= '0' && c <= '9') || c == '-')	// Si on a un caractère numérique,
+		{
+			fwscanf(fich, "%d", &value);	// on lit la valeure numérique.
+
+			if (hash == -1)		// Si c'est la première valeur numérique,
+			{
+				ajouterVille(px, value);	// on crée une nouvelle ville.
+				px = px->villeSuivante;
+
+				hash = 0; doll = 0;
+			}
+			else	// Ce n'est pas la première valeur du fichier.
+			{
+				if (sec == 0)	// Si on est dans la première section (numéros de villes).
+				{
+					if (hash == 1)	// Si il y a eu un seul '#' depuis l dernière valeur,
+					{
+						sub++;		// on change de sous section
+						ajouterVille(px, value);	// et on ajoute une ville.
+						px = px->villeSuivante;
+
+						hash = 0; value = 0;
+					}
+					else if (hash > 1)	// Si il y a eu plus d'un '#',
+					{
+						sec += hash / 2;	// On change de section (ajout de livraison dans une ville).
+											// On change de ville pour chaque paire de '#'.
+						sub = 0;	// La sous-section est réinitialisée.
+						px = debut;	// Il faut que l'insertion des livraison commence à la première ville,
+						for (int i = 0; i < hash / 2; i++)	// sauf si il y a plus d'une paire de '#'.
+						{
+							px = px->villeSuivante;
+						}
+
+						// Insertion de la livraison avec des valeurs par défaut.
+						ptLivraison py;
+						py = (ptLivraison)malloc(sizeof(tLivraison));
+						py->chauffeur = value;
+						py->capacite = 0;
+						py->disponible = 1;
+						py->enLivraison = NULL;
+						py->livraisonSuivante = px->listeLivraison;
+						px->listeLivraison = py;
+
+						hash = 0; doll = 0;
+					}
+				}
+				else if (sec > 0)	// Si on est pas dans la première section (ajout de livraison)
+				{
+					if (hash == 1)	// Si on a qu'un seul '#',
+					{
+						sub++;	// changement de sous-section (livraison différente pour une même ville)
+						champ = 0;	// et réinitialisation du champ de donné.
+
+						// Insertion de la livraison avec valeurs par défaut.
+						ptLivraison py;
+						py = (ptLivraison)malloc(sizeof(tLivraison));
+						py->chauffeur = value;
+						py->capacite = 0;
+						py->disponible = 1;
+						py->enLivraison = NULL;
+						py->livraisonSuivante = px->listeLivraison;
+						px->listeLivraison = py;
+
+						hash = 0; doll = 0;
+					}
+					else if (hash > 1)	// Si on a plus d'un '#',
+					{
+						sec += hash / 2;	// on change la ville où on inserre les livraisons
+						for (int i = 0; i < hash / 2; i++)	// éventuellement en sautant des villes si plus de 2 '#'.
+						{
+							px = px->villeSuivante;
+						}
+						champ = 0;
+
+						// Insertion de la livraison avec valeurs par défaut.
+						ptLivraison py;
+						py = (ptLivraison)malloc(sizeof(tLivraison));
+						py->chauffeur = value;
+						py->capacite = 0;
+						py->disponible = 1;
+						py->enLivraison = NULL;
+						py->livraisonSuivante = px->listeLivraison;
+						px->listeLivraison = py;
+
+						hash = 0; doll = 0;
+					}
+					else if (doll > 0)	// Si on a eu un '$' (ou plus),
+					{
+						champ += doll;	// on change de champ de donné.
+						
+						// Celon le champ actif:
+						if (champ == 1)
+						{
+							// changement de la capacité;
+							px->listeLivraison->capacite = value;
+						}
+						else if (champ == 2)
+						{
+							// changement de la disponibilité;
+							px->listeLivraison->disponible = value;
+						}
+						else if (champ == 3)
+						{
+							// ajout d'une destination.
+							ptVille pz;
+							pz = pdebut;
+
+							if (value > 0)
+							{
+								while (pz->villeSuivante != NULL)	// On parcours l'ensemble des villes chargées en mémoire,
+								{
+									if (pz->numVille == value)	// à la recherche de la ville avec le bon numéro.
+									{
+										px->listeLivraison->enLivraison = pz;	// Si c'est le cas on ajoute un pointeur vers cette ville.
+									}
+								}
+							}
+							else
+							{
+								px->listeLivraison->enLivraison = NULL;
+							}
+						}
+					}
+				}
+			}
+		}
+
+
+
+	}
+
 }
 
 
 
 
-
-int main() {
+int main()
+{
 	
 	// Création des bidons
 	ptVille pdebutVille = (ptVille) malloc (sizeof (tVille));
@@ -177,10 +348,11 @@ int main() {
 	int c = -1;
 	
 	// Boucle menu
-	while (c != 20) {
+	while (c != 20)
+	{
 		printf("1-	Ajouter Ville\n");
 		printf("2-	Modifier Ville\n");
-		printf("3-	Supprimer Ville");
+		printf("3-	Supprimer Ville\n");
 		printf("4-	Ajouter une nouvelle livraison dans une Ville\n");
 		printf("5-	Supprimer une livraison dans une Ville\n");
 		printf("6-	Modifier livraison dans une Ville\n");
@@ -189,36 +361,42 @@ int main() {
 		printf("9-	Transfert d'un chauffeur d'une Ville 1 a une Ville 2\n");
 		printf("10-	Enregistrer fichier\n");
 		printf("11-	Charger fichier\n");
-		printf("20-	Quitter\n");
+		printf("20-	Quitter\n\n");
 		
 		printf("Entrez le numero de la fonction a appeler: ");
 		scanf("%d", &c);
 
-		// Boucle entrée non valide
-		while (c < 0 || c > 20) {
+		
+		while (c < 0 || c > 20)
+		{
+			// Boucle entrée non valide
 			printf("\n");
 			printf("Entrez le numero de la fonction a appeler: ");
 			scanf("%d\n", &c);
 		}
 		
 
-		if (c == 1) {
+		if (c == 1)
+		{
+			// Nouvelle ville
 			int nouvVal;
 			px = pdebutVille;
 			printf("Entrer le numero de la ville: ");
-			scanf("%d\n", &nouvVal);
+			scanf("%d", &nouvVal);
 			ajouterVille(px, nouvVal);
 			px = pdebutVille->villeSuivante;
 			afficherListeVille(px);
 		}
 
 
-		else if (c == 2) {
+		else if (c == 2)
+		{
+			// Modification ville
 			int nouvVal, ancienneVal;
 			printf("Entrer le numero de la ville actuelle: ");
-			scanf("%d\n", &ancienneVal);
+			scanf("%d", &ancienneVal);
 			printf("Entrer le nouveau numero de la ville: ");
-			scanf("%d\n", &nouvVal);
+			scanf("%d", &nouvVal);
 			px = pdebutVille->villeSuivante;
 			modifierVille(px, ancienneVal, nouvVal);
 			px = pdebutVille->villeSuivante;
@@ -226,10 +404,12 @@ int main() {
 		}
 
 
-		else if (c == 3) {
+		else if (c == 3)
+		{
+			// Supression Ville
 			int valSupp;
 			printf("Entrer le numero de la ville a supprimer: ");
-			scanf("%d\n", &valSupp);
+			scanf("%d", &valSupp);
 			px = pdebutVille->villeSuivante;
 			supprimerVille(px, valSupp);
 			px = pdebutVille->villeSuivante;
@@ -237,47 +417,54 @@ int main() {
 		}
 
 
-		else if (c == 4) {
-
+		else if (c == 4)
+		{
+			// Ajout livraison
 		}
 
 
-		else if (c == 5) {
-
+		else if (c == 5)
+		{
+			// Suppression livraison
 		}
 
 
-		else if (c == 6) {
-
+		else if (c == 6)
+		{
+			// Modifier livraison
 		}
 
 
-		else if (c == 7) {
-
+		else if (c == 7)
+		{
+			// Recherche capacité dans ville
 		}
 
 
-		else if (c == 8) {
-
+		else if (c == 8)
+		{
+			// Recherche capacité partout
 		}
 
 
-		else if (c == 9) {
-
+		else if (c == 9)
+		{
+			// Transfert chaufeur
 		}
 
 
-		else if (c == 10) {
-
+		else if (c == 10)
+		{
+			// Enregistre fichier
 		}
 		
 		
-		else if (c == 11) {
-
+		else if (c == 11)
+		{
+			// Chargement fichier
+			chargerStructure(pdebutVille, px);
 		}
 	}
 	
 	return 1;
 }
-
-
